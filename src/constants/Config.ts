970a4489c5465234beb7867ada40bef487b9a4bd
Berkan Türkoğlu <berkan.trkgl35@gmail.com>
2026-03-20 19:11:16 +0300

    Make dev environment to run also in mobile device

--- src/constants/Config.ts
+++ src/constants/Config.ts
@@ -1,10 +1,23 @@
 // constants/Config.ts
 
-// Eğer Android emülatörde "Network Error" alırsan burayı "http://10.0.2.2:8001" yap.
-// iOS simülatör için "http://127.0.0.1:8001" veya "localhost" iyidir.
-export const FASTAPI_URL = "http://127.0.0.1:8001";
+import Constants from "expo-constants";
 
-const BASE_URL = "http://127.0.0.1:8000";
+// true → fiziksel cihazda test (Expo Go), bilgisayarın local IP'sini kullanır
+// false → simülatörde test, 127.0.0.1 kullanır
+const USE_PHYSICAL_DEVICE = true;
+
+const getLocalIP = () => {
+  const debuggerHost =
+    Constants.expoConfig?.hostUri ??
+    Constants.manifest2?.extra?.expoGo?.debuggerHost;
+  return debuggerHost?.split(":")[0] ?? "127.0.0.1";
+};
+
+const HOST = USE_PHYSICAL_DEVICE ? getLocalIP() : "127.0.0.1";
+
+export const FASTAPI_URL = `http://${HOST}:8001`;
+const BASE_URL = `http://${HOST}:8000`;
+
 export const API_URL = `${BASE_URL}/api`;
 
 export const COLORS = {
